5f6f93c56bfb8c5117c9ce51f3fa0fcaf0d45299
Sebastian August Berg <sebasabe@stud.ntnu.no>
2024-03-08 17:21:04 +0100

    Add pooling and second convolution layer

--- time_series_forecasting/models/cnn_forecasting_model.py
+++ time_series_forecasting/models/cnn_forecasting_model.py
@@ -7,15 +7,18 @@ class CNNForecastingModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv1d(in_channels=2, out_channels=64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
         self.pool = nn.AvgPool1d(kernel_size=2)
         self.flatten = nn.Flatten()
-        self.dense1 = nn.Linear(64 * 12, 50)
+        self.dense1 = nn.Linear(128 * 6, 50)
         self.dense2 = nn.Linear(50, 1)
 
     def forward(self, x):
         # Permute x from [batch_size, sequence_length, num_channels] to [batch_size, num_channels, sequence_length]
         x = x.permute(0, 2, 1)
         x = torch.relu(self.conv1(x))
+        x = self.pool(x)
+        x = torch.relu(self.conv2(x))
         x = self.pool(x)
         x = self.flatten(x)
         x = torch.relu(self.dense1(x))
