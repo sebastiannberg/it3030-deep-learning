b2e923147cff773786b318ab28281e81132844a4
Sebastian August Berg <sebasabe@stud.ntnu.no>
2024-04-10 14:38:06 +0200

    Anomaly detection autoencoder

--- deep_generative_models/main_autoencoder.py
+++ deep_generative_models/main_autoencoder.py
@@ -12,10 +12,12 @@ from utils.stacked_mnist import StackedMNISTData, DataMode
 from utils.verification_net import VerificationNet
 from utils.visualization import visualize_images, visualize_reconstructions
 
-# Setup
+# SETUP
+MODE = "mono" # Options: "mono", "color"
 TRAIN = False
-MODE = "color" # Options: "mono", "color"
-MODEL_FILENAME = "autoencoder_color_1712701349" # Set TRAIN to False to load
+MODEL_FILENAME = "mono_1712696920" # Set TRAIN to False to load
+TRAIN_ANOMALY = False
+ANOMALY_MODEL_FILENAME = "mono_missing_1712752424" # Set TRAIN_ANOMALY to False to load
 MONO_ENCODING_DIM = 16
 COLOR_ENCODING_DIM = 128
 NUM_EPOCHS = 10
@@ -24,10 +26,14 @@ CURRENT_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 DATA_PATH = os.path.join(CURRENT_DIR_PATH, "data")
 mono_dataset = StackedMNISTData(root=DATA_PATH, mode=DataMode.MONO | DataMode.BINARY)
 color_dataset = StackedMNISTData(root=DATA_PATH, mode=DataMode.COLOR | DataMode.BINARY)
+mono_missing_dataset = StackedMNISTData(root=DATA_PATH, mode=DataMode.MONO | DataMode.BINARY | DataMode.MISSING)
+color_missing_dataset = StackedMNISTData(root=DATA_PATH, mode=DataMode.COLOR | DataMode.BINARY | DataMode.MISSING)
 mono_data_loader = DataLoader(mono_dataset, batch_size=16, shuffle=True)
 color_data_loader = DataLoader(color_dataset, batch_size=16, shuffle=True)
+mono_missing_data_loader = DataLoader(mono_missing_dataset, batch_size=16, shuffle=True)
+color_missing_data_loader = DataLoader(color_missing_dataset, batch_size=16, shuffle=True)
 
-# Train Model
+# TRAIN MODEL
 def train(model, optimizer, loss_function, data_loader, num_epochs):
     print("\033[1;32m" + "="*15 + " Training " + "="*15 + "\033[0m")
     start_time = time.time()
@@ -63,12 +69,12 @@ loss_function = nn.MSELoss()
 
 if TRAIN:
     train(autoencoder, optimizer, loss_function, data_loader, num_epochs=NUM_EPOCHS)
-    torch.save(autoencoder.state_dict(), os.path.join(CURRENT_DIR_PATH, "saved_models", f"autoencoder_{MODE}_{int(datetime.now().timestamp())}"))
+    torch.save(autoencoder.state_dict(), os.path.join(CURRENT_DIR_PATH, "saved_models", "autoencoder", f"{MODE}_{int(datetime.now().timestamp())}"))
 else:
     print("Loading model...")
-    autoencoder.load_state_dict(torch.load(os.path.join(CURRENT_DIR_PATH, "saved_models", MODEL_FILENAME)))
+    autoencoder.load_state_dict(torch.load(os.path.join(CURRENT_DIR_PATH, "saved_models", "autoencoder", MODEL_FILENAME)))
 
-# Check Accuracy
+# CHECK ACCURACY
 verification_net_path = os.path.join(CURRENT_DIR_PATH, "utils", "saved_weights", "mono_float_complete.weights.h5")
 net = VerificationNet(file_name=verification_net_path)
 
@@ -80,7 +86,7 @@ all_images = torch.cat(all_images, dim=0)
 all_targets = torch.cat(all_targets, dim=0)
 with torch.no_grad():
     all_predictions = autoencoder(all_images)
-# visualize_reconstructions(all_images, all_predictions)
+visualize_reconstructions(all_images, all_predictions)
 
 if MODE == "mono":
     tolerance = 0.8
@@ -90,13 +96,12 @@ elif MODE == "color":
     # Correctly order channels for hundreds, tens, and ones
     all_predictions = torch.flip(all_predictions, (3,))
     predictability, acc = net.check_predictability(data=all_predictions, correct_labels=all_targets, tolerance=tolerance)
-
 if predictability != None:
     print(f"Predictability: {100 * predictability:.2f}%")
 if acc != None:
     print(f"Accuracy: {100 * acc:.2f}%")
 
-# Generate Examples
+# GENERATE NEW IMAGES
 with torch.no_grad():
     x = all_images.permute(0, 3, 1, 2)
     x = autoencoder.encoder(x)
@@ -108,13 +113,56 @@ mean = np.mean(x, axis=0)
 std = np.std(x, axis=0)
 # Generate random encodings with mean and std
 if MODE == "mono":
-    z = np.random.normal(mean, std, (16, MONO_ENCODING_DIM)).astype(np.float32)
+    z = np.random.normal(mean, std, (1000, MONO_ENCODING_DIM)).astype(np.float32)
 elif MODE == "color":
-    z = np.random.normal(mean, std, (16, COLOR_ENCODING_DIM)).astype(np.float32)
+    z = np.random.normal(mean, std, (1000, COLOR_ENCODING_DIM)).astype(np.float32)
 z_tensor = torch.from_numpy(z)
 with torch.no_grad():
     generated = autoencoder.expand(z_tensor)
     generated = generated.view(-1, 64, 4, 4)
     generated = autoencoder.decoder(generated)
     generated = generated.permute(0, 2, 3, 1)
-visualize_images(title="Generated Images", images=generated)
+if MODE == "mono":
+    predictability, _ = net.check_predictability(data=generated, tolerance=0.8)
+    coverage = net.check_class_coverage(data=generated, tolerance=0.8)
+elif MODE == "color":
+    predictability, _ = net.check_predictability(data=generated, tolerance=0.5)
+    coverage = net.check_class_coverage(data=generated, tolerance=0.5)
+if predictability != None:
+    print(f"Predictability: {100 * predictability:.2f}%")
+if coverage != None:
+    print(f"Coverage: {100 * coverage:.2f}%")
+visualize_images(title="Generated Images", images=generated[:16])
+
+# ANOMALY DETECTION
+if MODE == "mono":
+    anomaly_autoencoder = Autoencoder(channels=1, encoding_dim=MONO_ENCODING_DIM)
+    data_loader = mono_missing_data_loader
+elif MODE == "color":
+    anomaly_autoencoder = Autoencoder(channels=3, encoding_dim=COLOR_ENCODING_DIM)
+    data_loader = color_missing_data_loader
+else:
+    raise ValueError("MODE must be either 'mono' or 'color'")
+
+optimizer = optim.Adam(anomaly_autoencoder.parameters(), lr=LEARNING_RATE)
+loss_function = nn.MSELoss()
+
+if TRAIN_ANOMALY:
+    train(anomaly_autoencoder, optimizer, loss_function, data_loader, num_epochs=NUM_EPOCHS)
+    torch.save(anomaly_autoencoder.state_dict(), os.path.join(CURRENT_DIR_PATH, "saved_models", "autoencoder", f"{MODE}_missing_{int(datetime.now().timestamp())}"))
+else:
+    print("Loading model...")
+    anomaly_autoencoder.load_state_dict(torch.load(os.path.join(CURRENT_DIR_PATH, "saved_models", "autoencoder", ANOMALY_MODEL_FILENAME)))
+
+print("Calculating reconstruction loss...")
+losses = []
+with torch.no_grad():
+    for image in all_images:
+        image = image[None, :, :, :]  # Ensure image has a batch dimension
+        reconstruction = anomaly_autoencoder(image)
+        reconstruction_loss = loss_function(reconstruction, image).item()
+        losses.append((image, reconstruction_loss))
+sorted_losses = sorted(losses, key=lambda x: x[1], reverse=True)
+print([x[1] for x in sorted_losses[:16]])
+most_anomalous_images = torch.cat([x[0] for x in sorted_losses], dim=0)[:16]
+visualize_images("Anomaly Detection", most_anomalous_images)
